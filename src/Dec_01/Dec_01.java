68e3b287867f76964d7de1c448f33943e6420d67
Rostigpepe <Robin.olivemark@gmail.com>
2021-12-03 14:48:20 +0100

    Added comments

--- src/Dec_01/Dec_01.java
+++ src/Dec_01/Dec_01.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public class Dec_01 {
 
-    private static List<Integer> allNumbers = new ArrayList<>();
+    private static final List<Integer> allNumbers = new ArrayList<>();
+
 
     public static void main(String[] args) {
         readFileIntoList("src/Dec_01/puzzleInput01.txt");
@@ -16,11 +17,18 @@ public class Dec_01 {
         amountOfGroupIncrements();
     }
 
+    /**Little method to quickly get the sum of a group of depths
+     * @param index Which index we're going from in allNumbers
+     * @return Returns sum of the group
+     */
     public static int getGroupSum(int index){
         return allNumbers.get(index) + allNumbers.get(index + 1) + allNumbers.get(index + 2);
     }
 
 
+    /**Method to read the whole input file into a single list for easier management
+     * @param filepath Path of the file we read the input from
+     */
     public static void readFileIntoList(String filepath){
 
         try(BufferedReader fileIn = new BufferedReader(new FileReader(filepath))){
@@ -38,6 +46,9 @@ public class Dec_01 {
     }
 
 
+    /**Method to count the amount of times one number is higher than the last one in the list
+     * Takes one index, compares it to the next one, reassigns and repeats for the whole list
+     */
     public static void amountOfIncrements(){
         int amountOfIncrements = 0;
         int currentDepth = allNumbers.get(0);
@@ -55,6 +66,9 @@ public class Dec_01 {
     }
 
 
+    /**Method to count increments between groups of three depths
+     * Gets a group based on index, takes the next group starting at index + 1, compares, repeats
+     */
     public static void amountOfGroupIncrements(){
         int amountOfGroupIncrements = 0;
 
@@ -66,7 +80,6 @@ public class Dec_01 {
                 amountOfGroupIncrements++;
             }
         }
-
         System.out.println(amountOfGroupIncrements);
     }
 }
