047343d8182bf038a96cf8c229571f5142adbb48
Niklas Sankell <niklas_san@gmail.com>
2021-12-06 10:09:46 +0100

    Solution Dec_03 part2

--- src/Dec_03/Dec_03.java
+++ src/Dec_03/Dec_03.java
@@ -8,10 +8,15 @@ import java.util.List;
 public class Dec_03 {
 
     private List<String> binaryInput = new ArrayList<>();
+    private List<String> oxygenRatings = new ArrayList<>();
+    private List<String> co2Ratings = new ArrayList<>();
     private StringBuilder binaryGammaRate = new StringBuilder("");
     private StringBuilder binaryEpsilonRate = new StringBuilder("");
     private int decimalGammaRate = 0;
     private int decimalEpsilonRate = 0;
+    private int decimalOxygenRating = 0;
+    private int decimalCo2Rating = 0;
+    private int diagnostics;
     private int consumption;
 
     public static void main(String[] args) {
@@ -22,11 +27,12 @@ public class Dec_03 {
         readFile();
         getRatesFromInput();
         calculateConsumption();
-        System.out.println(binaryGammaRate);
-        System.out.println(binaryEpsilonRate);
-        System.out.println(decimalGammaRate);
-        System.out.println(decimalEpsilonRate);
-        System.out.println(consumption);
+        findOxygenRating();
+        findCO2Rating();
+        calculateRatings();
+        System.out.println(oxygenRatings);
+        System.out.println(co2Ratings);
+        System.out.println(diagnostics);
     }
 
     public void readFile() {
@@ -34,6 +40,8 @@ public class Dec_03 {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/Dec_03/puzzleInput03.txt"));) {
             while ((readLine = bufferedReader.readLine()) != null) {
                 binaryInput.add(readLine);
+                oxygenRatings.add(readLine);
+                co2Ratings.add(readLine);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -59,6 +67,64 @@ public class Dec_03 {
                 binaryEpsilonRate.append("0");
             }
         }
+    }
+
+    public void findOxygenRating() {
+        for (int i = 0; i < 12; i++) {
+            int countedZeroes = 0;
+            int countedOnes = 0;
+            for (String binary : oxygenRatings) {
+                if (binary.charAt(i) == '0') {
+                    countedZeroes++;
+                } else if (binary.charAt(i) == '1') {
+                    countedOnes++;
+                }
+            }
+            if (oxygenRatings.size() > 1) {
+                if (countedOnes < countedZeroes) {
+                    int finalI2 = i;
+                    oxygenRatings.removeIf(binary -> binary.charAt(finalI2) == '1');
+                } else if (countedOnes == countedZeroes) {
+                    int finalI1 = i;
+                    oxygenRatings.removeIf(binary -> binary.charAt(finalI1) == '0');
+                } else {
+                    int finalI = i;
+                    oxygenRatings.removeIf(binary -> binary.charAt(finalI) == '0');
+                }
+            }
+        }
+    }
+
+    public void findCO2Rating() {
+        for (int i = 0; i < 12; i++) {
+            int countedZeroes = 0;
+            int countedOnes = 0;
+            for (String binary : co2Ratings) {
+                if (binary.charAt(i) == '0') {
+                    countedZeroes++;
+                } else if (binary.charAt(i) == '1') {
+                    countedOnes++;
+                }
+            }
+            if (co2Ratings.size() > 1) {
+                if (countedOnes < countedZeroes) {
+                    int finalI1 = i;
+                    co2Ratings.removeIf(binary -> binary.charAt(finalI1) == '0');
+                } else if (countedOnes == countedZeroes) {
+                    int finalI = i;
+                    co2Ratings.removeIf(binary -> binary.charAt(finalI) == '1');
+                } else {
+                    int finalI2 = i;
+                    co2Ratings.removeIf(binary -> binary.charAt(finalI2) == '1');
+                }
+            }
+        }
+    }
+
+    public void calculateRatings() {
+        decimalOxygenRating = Integer.parseInt(oxygenRatings.get(0),2);
+        decimalCo2Rating = Integer.parseInt(co2Ratings.get(0),2);
+        diagnostics = decimalOxygenRating * decimalCo2Rating;
     }
 
     public void calculateConsumption() {
